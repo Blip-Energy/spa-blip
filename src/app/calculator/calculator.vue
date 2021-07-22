6fa7b00d17541be478d8d23f28dfa5c8a97a5364
xiushiwang <xiushiwang2020@u.northwestern.edu>
2021-07-21 19:39:57 -0500

    Updates on Jul 21st

--- src/app/calculator/calculator.vue
+++ src/app/calculator/calculator.vue
@@ -60,8 +60,8 @@
 <script>
 import banner from "../shared/components/banner.vue";
 import savings from "../savings.vue";
-import graph from "./graph.vue";
-import signUp from "@/app/shared/components/signUpMailChimp.vue";
+import graph from "./graphI.vue";
+import signUp from "@/app/shared/components/signUp.vue";
 import utility from "./utility.vue"
 import noneInputFooter from "@/app/shared/components/noneInputFooter";
 
---
+++ src/app/shared/components/homeII.vue
@@ -0,0 +1,148 @@
+<template>
+  <div>
+    <banner></banner>
+    <div class="main">
+      <!--start: Energy storage for anyone, anywhere-->
+      <section class="SavingRentersMoney b-DEF5FF">
+        <div class="innerFog">
+          <img src="../../../assets/webPages/landing/LandingPageHeroImage.png" class="hero">
+          <b-row class="insider m-tb-a t-left">
+            <h1 class="c-254B77 t-left">Energy storage for anyone, anywhere</h1>
+            <b-row class="m-l-0 p-l-0 aSustainable">
+              <p class="p1point5 c-254B77 t-left p-l-0 f-weight-400">
+                Everyone deserves access to energy storage, so we’re building blipOne, the world’s most accessible smart home battery.
+                <br>Compact, portable and affordable with a sleek, modern design.
+              </p>
+              <button type="button" class="btn submit" onclick="window.location.href='../product'">
+                Learn more
+              </button>
+            </b-row>
+          </b-row>
+        </div>
+      </section>
+      <!--end: Energy storage for anyone, anywhere-->
+
+      <!--start: Pioneering a new era of energy storage-->
+      <section class="AboutBlip b-FFFFFF i-a-c">
+        <b-row class="w-90p insider">
+          <b-col  xs="12" sm="12" md="7" lg="7" xl="7" class="leftImg p-l-0">
+            <img src="../../../assets/webPages/landing/BlipEnvironment-03.png" alt="" class="w-100p">
+          </b-col>
+          <b-col  xs="12" sm="12" md="5" lg="5" xl="5" class="rightText t-left">
+            <p class="p1point5 c-4F9BC1" style="margin-bottom: 12px">A NEW KIND OF BATTERY</p>
+            <h2 class="h2point8 c-254B77 f-weight-400 m-l-0 p-l-0" style="margin-bottom: 50px">Pioneering a new era of energy storage</h2>
+            <p class="p3 c-183B56 m-l-0 p-l-0">
+              Get reliable backup power and bill savings while fighting climate change and stabilizing the grid, all without turning off the air conditioning or lights.
+              <br><br>
+              blipOne is an affordable smart battery that makes it easier than ever for anyone to access energy storage, at a cost that can pay for itself in savings and without the hassle of professional installation.
+            </p>
+            <button type="button" class="btn submit transparent c-4F9BC1" onclick="window.location.href='../product'">
+              Learn More about blipOne <font-awesome-icon icon="arrow-right" style="font-size: 16px"/>
+            </button>
+          </b-col>
+        </b-row>
+      </section>
+      <!--end: Pioneering a new era of energy storage-->
+
+      <!--start: Stop paying for expensive electricity-->
+      <section class="cleanEnergy b-EEF9FE i-a-c t-center">
+        <b-row class="w-90p insider t-center">
+          <b-col  xs="12" sm="12" md="5" lg="5" xl="5" class="leftText t-left">
+            <p class="p1point5 c-4F9BC1" style="margin-bottom: 12px">SAVE, EFFORTLESSLY</p>
+            <h2 class="h2point8 c-254B77 f-weight-400 m-l-0 p-l-0" style="margin-bottom: 50px">Stop paying for expensive electricitye</h2>
+            <p class="p3 c-183B56 m-l-0 p-l-0">
+              Our smart battery blipOne has the brains to save you money on your monthly electric bill by taking advantage of electricity prices that change throughout the day.
+              <br><br>
+              By storing energy overnight when the price is low, and using it to power your devices during the day when the price is highest, blip saves you money without having to turn anything off.
+            </p>
+            <button type="button" class="btn submit" onclick="window.location.href='../product'">
+              Savings Calculator
+            </button>
+          </b-col>
+          <b-col  xs="12" sm="12" md="7" lg="7" xl="7" class="rightImg p-l-0">
+            <img src="../../../assets/webPages/landing/Time-of-Use-Rate-Plans_v02.png" alt="" class="w-100p">
+          </b-col>
+        </b-row>
+      </section>
+      <!--end: Stop paying for expensive electricity-->
+
+      <!--start: Fight climate change from your couch-->
+      <section class="blipHelps b-254B77 i-a-c">
+        <b-row class="w-90p insider">
+          <b-col  xs="12" sm="12" md="7" lg="7" xl="7" class="leftImg p-l-0">
+            <img src="../../../assets/webPages/landing/earth.png" alt="" class="w-90p">
+          </b-col>
+          <b-col  xs="12" sm="12" md="5" lg="5" xl="5" class="rightText t-left">
+            <p class="p1point5 c-DEF5FF" style="margin-bottom: 12px">SUSTAINABILITY</p>
+            <h2 class="h2point8 c-FFFFFF f-weight-400 m-l-0 p-l-0" style="margin-bottom: 50px">Fight climate change from your couch</h2>
+            <p class="p3 c-FFFFFF m-l-0 p-l-0">
+              Imagine a world free from fuel-burning power plants. With creative solutions to volatile energy demand, it’s possible
+              <br><br>
+              Even with more renewable energy sources on the grid, we still need old school fuel-burning “peaker” power plants to meet demand when the sun isn’t shining or wind isn’t blowing, or during unexpected spikes
+              in demand.
+              <br><br>
+              Power grids are most strained when the sun goes down and people get home from work. By charging overnight and using that power during the daily peak times, blip reduces day-to-day reliance on peaker power plants.
+              <br><br>
+              It’s true, one blip won’t change the world, but a community full of blips can work to decommission a dirty power plant one home at a time. If we each do a little, we can all do a lot
+            </p>
+          </b-col>
+        </b-row>
+      </section>
+      <!--end: Fight climate change from your couch-->
+
+<!--      <p>-->
+<!--        Click <a href="#" @click.prevent="disableTracking">here</a>,-->
+<!--        to disable the tracking through Google Analytics.-->
+<!--      </p>-->
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import banner from "./banner.vue";
+import transparentBanner from "@/app/shared/components/transparentBanner";
+import Footer from "./FooterII.vue";
+// import Footer from "./Footer.vue";
+import image from "../../../assets/alain_duchateau_QvZj7r3efC4_unsplash_lighterblue_Copy.jpg"
+import image2 from "../../../assets/blip-mockup-updateForHome.png"
+
+export default {
+  name: "home",
+  data() {
+    return {
+      image: image,
+      image2: image2
+    };
+  },
+  mounted() {
+    this.$ga.page('/');
+    this.$ga.page('/home');
+  },
+
+  methods: {
+    disableTracking: function() {
+      this.$ga.disable();
+      alert('Tracking disabled');
+    },
+    click: function() {
+      this.$ga.event('category', 'action', 'label', 123)
+    }
+  },
+  computed: {
+  },
+  components: {
+    banner,
+    // transparentBanner,
+    // FontAwesomeIcon,
+    Footer
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "src/scss/pages/common";
+@import "src/scss/pages/homeII";
+</style>
+
+
